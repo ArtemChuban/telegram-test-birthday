83a2bfc77e2ec27b18d586424003cc80c257493f
Artem Chuban <ArtemChuban@yandex.ru>
2025-03-10 12:21:22 +0300

    [Backend] Docstrings

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -21,6 +21,25 @@ def get_current_user(
     auth_cred: HTTPAuthorizationCredentials = Depends(telegram_authentication_schema),
     telegram_authenticator: TelegramAuthenticator = Depends(get_telegram_authenticator),
 ) -> WebAppUser:
+    """
+    Retrieve the current user based on the provided Telegram authentication credentials.
+
+    This function validates the authentication credentials using the Telegram authenticator.
+    If the credentials are valid and the user is found, the corresponding user object is returned.
+    If the credentials are invalid or the user is not found, appropriate HTTP exceptions are raised.
+
+    Args:
+        auth_cred (HTTPAuthorizationCredentials): The authentication credentials extracted from the request.
+        telegram_authenticator (TelegramAuthenticator): The Telegram authenticator used to validate the credentials.
+
+    Raises:
+        HTTPException:
+            - If the authentication credentials are invalid or cannot be processed, a 403 Forbidden error is raised.
+            - If an internal error occurs during validation, a 500 Internal Server Error is raised.
+
+    Returns:
+        WebAppUser: The authenticated user object associated with the provided credentials.
+    """
     try:
         init_data = telegram_authenticator.validate(unquote(auth_cred.credentials))
     except InvalidInitDataError:
--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -9,11 +9,18 @@ from telegram_webapp_auth.data import WebAppUser
 router = APIRouter(prefix="/users")
 
 
-@router.post("/")
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(
     user: WebAppUser = Depends(get_current_user),
     birthday: date = Body(embed=True),
 ) -> UserSchema:
+    """
+    Create a new user in the system based on the provided user information and birthday.
+
+    This endpoint allows the creation of a new user if the user does not already exist in the database.
+    It requires the authenticated user's information and their birthday. If a user with the same ID already exists,
+    a 409 Conflict error is raised.
+    """
     if await Users.exists(id=user.id):
         raise HTTPException(status.HTTP_409_CONFLICT)
     instance = await Users.create(
@@ -30,6 +37,13 @@ async def create_user(
 async def get_me(
     user: WebAppUser = Depends(get_current_user),
 ) -> UserSchema:
+    """
+    Retrieve the authenticated user's information.
+
+    This endpoint returns the details of the currently authenticated user.
+    It uses the user's ID to fetch their information from the database.
+    If the user is not found, a 404 Not Found error is raised.
+    """
     instance = await Users.get_or_none(id=user.id)
     if instance is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND)
@@ -40,6 +54,12 @@ async def get_me(
 async def get_user(
     user_id: int = Path(),
 ) -> UserSchema:
+    """
+    Retrieve a user's information by their user ID.
+
+    This endpoint fetches the details of a user specified by the user ID in the path parameter.
+    If the user is not found in the database, a 404 Not Found error is raised.
+    """
     instance = await Users.get_or_none(id=user_id)
     if instance is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND)
@@ -51,6 +71,13 @@ async def update_birthday(
     user: WebAppUser = Depends(get_current_user),
     birthday: date = Body(embed=True),
 ) -> UserSchema:
+    """
+    Update the authenticated user's birthday.
+
+    This endpoint allows the authenticated user to update their birthday.
+    It retrieves the user's information from the database and updates the birthday field.
+    If the user is not found, a 404 Not Found error is returned.
+    """
     instance = await Users.get_or_none(id=user.id)
     if instance is None:
         return HTTPException(status.HTTP_404_NOT_FOUND)
